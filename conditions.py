b3c13d04bff0c2ff1c345caf6a9c639f3e80a8b8
claux66 <159801719+claux66@users.noreply.github.com>
2024-02-15 18:07:21 -0500

    This is my input and conditions program assignment

---
+++ conditions.py
@@ -0,0 +1,19 @@
+#CS 104
+
+#Callan Laux
+
+#conditions.py
+
+temp = int(input("Please Enter Temperature:   "))
+
+if temp > 90:
+    print("Wear Shorts")
+elif temp > 70:
+    print("Short Sleeves are fine")
+elif temp > 50:
+    print("Wear a jacket")
+elif temp > 32:
+    print("Wear a heavy coat")
+else:
+    print("Stay Inside")
+          
